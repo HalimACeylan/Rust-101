cbf2af601f3638b15d36e91eea9b109dbc091e90
Halim Ceylan <halimceylan7@gmail.com>
2025-01-02 23:22:00 +0100

    end of the chapter 3 big milestone

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,59 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+pub struct Order {
+    product_name:String,
+    quantity:u32,
+    unit_price:u32,
+}
+
+impl Order {
+
+    pub fn isValid(product_name:&String , quantity:&u32 , unit_price:&u32) {
+        if product_name.is_empty() {
+            panic!("product name cannot be the empty");
+        }
+        if product_name.len() > 300 {
+            panic!("product name cannot be the bigger than 300");
+        }
+        if *quantity <= 0 {
+            panic!("quantity smaller than zero");
+        }
+        if *unit_price <= 0 {
+            panic!("unit price smaller than zero");
+        }
+
+    }
+    pub fn new(product_name:String , quantity:u32 , unit_price:u32) -> Order {
+        Order::isValid(&product_name,&quantity,&unit_price);
+        Order {
+            product_name,
+            quantity,
+            unit_price
+        }
+    }
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+    pub fn set_product_name(&mut self,product_name:String){
+        Order::isValid(&product_name,&self.quantity,&self.unit_price);
+        self.product_name = product_name;
+    }
+    pub fn set_unit_price(&mut self,unit_price:u32){
+        Order::isValid(&self.product_name,&self.quantity,&unit_price);
+        self.unit_price = unit_price;
+    }
+    pub fn set_quantity(&mut self,quantity:u32){
+        Order::isValid(&self.product_name,&quantity,&self.unit_price);
+        self.quantity = quantity;
+    }
+}
